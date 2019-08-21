eb98d8ef3574d1bf2e2ea273af26442f6876cf01
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-08-22 09:18:23 +1200

    added in new modules

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
@@ -16,6 +22,30 @@ app.get('/', function(req, res){
 app.get('/allProducts', function(req, res){
     res.send(allProducts);
 })
+
+
+app.get('/product/:id', function(req, res){
+    const id = req.params.id
+    for (var i = 0; i < allProducts.length; i++) {
+        if(id == allProducts[i].id){
+            res.send(allProducts[i]);
+            break;
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
