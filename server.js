a8a9f496ab44dbad072773bc62b0ceadb6b4ef97
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-08-23 11:37:45 +1200

    connected to mongo db and can create new products

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./config.json');
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0-zd20o.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('we are connected to mongo db');
+});
 
 const allProducts = require('./data/products');
 
@@ -34,15 +46,27 @@ app.get('/product/:id', function(req, res){
     }
 })
 
+
+const Product = require('./models/products');
+
 app.post('/product', function(req, res){
     // console.log('a post request has been made');
     // console.log(req.body);
-    let product = {
+    // let product = {
+    //     name: req.body.name,
+    //     price: req.body.price,
+    //     message: 'We are about to send this product to a database'
+    // };
+    // res.send(product);
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price,
-        message: 'We are about to send this product to a database'
-    };
-    res.send(product);
+        price: req.body.price
+    });
+
+    product.save().then(result => {
+        res.send(result);
+    }).catch(err => res.send(err));
 });
 
 
